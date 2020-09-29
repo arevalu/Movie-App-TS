749bd5558e01184d2c29699c9a51d8ef38a65f0f
Luciana <arevalolucianadg@gmail.com>
2020-09-29 20:25:42 -0300

    Ajustes en Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HeaderWrapper } from './styled';
 
 const Header: React.FC = () => {
+
     return (
         <HeaderWrapper>
             <h1>Movie App</h1>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
-    
+    background-color: '';
+    height: 80px;
     width: 100%;
 `
