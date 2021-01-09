2ce599ed4ffeab26699cbf38848a09f330a40a05
Luciana <arevalolucianadg@gmail.com>
2021-01-08 22:16:13 -0300

    Refactor en getPopular

--- src/services/getPopular.ts
+++ src/services/getPopular.ts
@@ -1,14 +1,28 @@
 import { URL_API, API_TOKEN } from '../utils/const';
 import { popularMovies } from '../types/movies';
+import { getGenres } from '../utils/getGenres';
 
-export const getPopular = ( type: string | undefined = 'movie' ): Promise<popularMovies> => {
+export const getPopular = async ( type: string | undefined = 'movie' ): Promise<popularMovies> => {
 
-    return fetch(`${ URL_API }${ type }/popular?${ API_TOKEN }`)
-        .then( res => res.json() )
-        .then( data => {
-            const { results } = data;
-            return results;
-        } )
-        .catch( err => console.log( err ))
+    const res = await fetch(`${ URL_API }${ type }/popular?${ API_TOKEN }`);
+    const { results } = await res.json();
+
+    const movies = results.map( (movie: any) => {
+
+        let genres_movie: string[] = [];
+        movie.genre_ids.map( (genre: number) => genres_movie.push( getGenres( genre ) ));
+        
+        return {
+            backdrop_path: movie.backdrop_path,
+            genre_ids: genres_movie,
+            id: movie.id,
+            overview: movie.overview,
+            poster_path: movie.poster_path,
+            title: movie.original_title,
+            vote_average: movie.vote_average,
+        }
+    });
+
+    return movies;
 
 }
